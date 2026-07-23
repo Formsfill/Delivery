b84ccab2a23edb8df5846de21e1746a869076a3d
Formsfill <muhammednizamudheenkk@gmail.com>
2026-07-23 10:03:44 +0300

    Refactor firebase.js to streamline imports and config

--- firebase.js
+++ firebase.js
@@ -2,20 +2,28 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/fireba
 
 import {
   getFirestore,
-  doc,
-  updateDoc,
+  collection,
   onSnapshot,
-  collection
+  doc,
+  updateDoc
 } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCmSLUHk9j82QFa_dOyJdAv-8O_MP9axn8",
+  authDomain: "arafiz-9a4d4.firebaseapp.com",
+  projectId: "arafiz-9a4d4",
+  storageBucket: "arafiz-9a4d4.firebasestorage.app",
+  messagingSenderId: "120289928910",
+  appId: "1:120289928910:web:23eb192ca977e9e49f529f",
+  measurementId: "G-ZR0S7EP1YE"
+};
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-
-
-
-
+// Live Seat Updates
 onSnapshot(collection(db, "seats"), (snapshot) => {
 
     snapshot.forEach((seatDoc) => {
@@ -43,29 +51,5 @@ onSnapshot(collection(db, "seats"), (snapshot) => {
 
 });
 
-
-
-
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyCmSLUHk9j82QFa_dOyJdAv-8O_MP9axn8",
-    authDomain: "arafiz-9a4d4.firebaseapp.com",
-    projectId: "arafiz-9a4d4",
-    storageBucket: "arafiz-9a4d4.firebasestorage.app",
-    messagingSenderId: "120289928910",
-    appId: "1:120289928910:web:23eb192ca977e9e49f529f",
-    measurementId: "G-ZR0S7EP1YE"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+// Export Firestore
+export { db, doc, updateDoc };
