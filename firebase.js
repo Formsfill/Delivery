7cc1f4072b288633952637b34f8b69b7d24c256a
Formsfill <muhammednizamudheenkk@gmail.com>
2026-07-24 12:39:57 +0300

    Update Firebase configuration with new credentials
    Updated Firebase configuration with new project credentials.

--- firebase.js
+++ firebase.js
@@ -10,12 +10,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT.firebasestorage.app",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCmSLUHk9j82QFa_dOyJdAv-8O_MP9axn8",
+  authDomain: "arafiz-9a4d4.firebaseapp.com",
+  projectId: "arafiz-9a4d4",
+  storageBucket: "arafiz-9a4d4.firebasestorage.app",
+  messagingSenderId: "120289928910",
+  appId: "1:120289928910:web:23eb192ca977e9e49f529f",
+  measurementId: "G-ZR0S7EP1YE"
 };
 
 const app = initializeApp(firebaseConfig);
